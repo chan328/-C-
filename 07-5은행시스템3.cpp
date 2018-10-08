2c767a4ee554a92e4a91950db7a3628f7f101b98
chan328 <rkdcks020328@gmail.com>
2018-10-08 11:03:43 +0900

    01/08

--- 07-5은행시스템3.cpp
+++ 07-5은행시스템3.cpp
@@ -171,5 +171,32 @@ int main(void)
 	AccountHandler manager;
 	int choice;
 
+	while (1)
+	{
+		manager.ShowMenu();
+		cout << "¼±ÅÃ";
+		cin >> choice;
+		cout << endl;
 
+		switch (choice)
+		{
+		case MAKE:
+			manager.MakeAccount();
+			break;
+		case DEPOSIT:
+			manager.MakeAccount();
+			break;
+		case WITHDRAW:
+			manager.WithdrawMoney();
+			break;
+		case INQUIRE:
+			manager.ShowAllAccInfo();
+			break;
+		case EXIT:
+			return 0;
+		default:
+			cout << "illeager selection.." << endl;
+		}
+	}
+	return 0;
 }
